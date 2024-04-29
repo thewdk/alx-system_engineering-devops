ff44133b69c198ae7bd736eb7a87119b82ac2e53
Daniel Watchman <dannietortee@gmail.com>
2024-04-29 01:53:08 +0100

    update and modifygi

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
-# Installing puppet lint
-package { 'puppet-lint':
-  ensure   => '2.1.0',
-  provider => 'gem',
+# This resource installs a package called flask
+
+package {'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
 }
 
+package {'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+}
